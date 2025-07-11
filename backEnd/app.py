a1e7fe3036b1a0d62f9d0c8ae20959e8a6a7ba89
Moraleda <pabmoralv@alum.us.es>
2025-07-11 18:46:15 +0200

    lecturaDePuerto

--- backEnd/app.py
+++ backEnd/app.py
@@ -54,4 +54,5 @@ def combinar():
             return f"❌ Error al combinar etiquetas: {str(e)}", 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Render le da un puerto a tu app
+    app.run(host='0.0.0.0', port=port, debug=True)
